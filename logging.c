ebc12b5bbc5a8b72a1d03b0f2c15b7ab3af7e0f8
David Johnston <david@atavus.org>
2018-01-21 12:35:15 +1100

    Adjusted the logging format

--- logging.c
+++ logging.c
@@ -15,9 +15,8 @@ void logdebug(const char* fusecmd, const char* fmt, ...) {
   struct tm *tm = localtime(&current_time);
   char buf[30];
   asctime_r(tm,buf);
-  buf[strlen(buf)-1]=0;
-  fprintf(Y_STATE->logfile,"%s ",buf);
-  fprintf(Y_STATE->logfile,"%s: ",fusecmd);
+  fprintf(Y_STATE->logfile,"%s",buf);
+  fprintf(Y_STATE->logfile,": %s : ",fusecmd);
   vfprintf(Y_STATE->logfile,fmt,va);
   fprintf(Y_STATE->logfile,"\n");
   fflush(Y_STATE->logfile);
@@ -32,9 +31,8 @@ void loginfo(const char* fusecmd, const char* fmt, ...) {
   struct tm *tm = localtime(&current_time);
   char buf[30];
   asctime_r(tm,buf);
-  buf[strlen(buf)-1]=0;
-  fprintf(Y_STATE->logfile,"%s ",buf);
-  fprintf(Y_STATE->logfile,"%s: ",fusecmd);
+  fprintf(Y_STATE->logfile,"%s",buf);
+  fprintf(Y_STATE->logfile,": %s : ",fusecmd);
   vfprintf(Y_STATE->logfile,fmt,va);
   fprintf(Y_STATE->logfile,"\n");
   fflush(Y_STATE->logfile);
@@ -48,9 +46,8 @@ void logdata(const char* fusecmd, const unsigned char* data, size_t size) {
     struct tm *tm = localtime(&current_time);
     char buf[30];
     asctime_r(tm,buf);
-    buf[strlen(buf)-1]=0;
-    fprintf(Y_STATE->logfile,"%s ",buf);
-    fprintf(Y_STATE->logfile,"%s: ",fusecmd);
+    fprintf(Y_STATE->logfile,"%s",buf);
+    fprintf(Y_STATE->logfile,": %s : ",fusecmd);
     for(uint64_t ptr=0; ptr<size; ptr++) {
       fprintf(Y_STATE->logfile," %02x",data[ptr]);
     }
@@ -68,9 +65,8 @@ int logerr(const char* fusecmd, const char* fmt, ...) {
   struct tm *tm = localtime(&current_time);
   char buf[30];
   asctime_r(tm,buf);
-  buf[strlen(buf)-1]=0;
-  fprintf(Y_STATE->logfile,"%s ",buf);
-  fprintf(Y_STATE->logfile,"Error in %s [%d] %s\n\t",fusecmd,rc,strerror(errno));
+  fprintf(Y_STATE->logfile,"%s",buf);
+  fprintf(Y_STATE->logfile,": %s : Error [%d] %s\n\t",fusecmd,rc,strerror(errno));
   vfprintf(Y_STATE->logfile,fmt,va);
   fprintf(Y_STATE->logfile,"\n");
   fflush(Y_STATE->logfile);
